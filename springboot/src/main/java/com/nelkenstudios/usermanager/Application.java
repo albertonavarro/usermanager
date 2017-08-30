e2a9512586c043f5319877e8f129a1bb76cd6b8f
albertonavarro <alberto.cartagena@gmail.com>
2017-08-30 22:56:47 +0200

    Checkstyle, freemarker and static content

--- springboot/src/main/java/com/nelkenstudios/usermanager/Application.java
+++ springboot/src/main/java/com/nelkenstudios/usermanager/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public final class Application {
 
+    private Application() {
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
